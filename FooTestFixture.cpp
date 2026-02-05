f4a2d5baf4a2a496327082fb70617f957a257f0f
Venugopal K <venu.shastri@gmail.com>
2026-02-05 11:41:13 +0530

    value parameterized test

--- FooTestFixture.cpp
+++ FooTestFixture.cpp
@@ -1,8 +1,16 @@
 #include <iostream>
 using namespace std;
 #include <gtest/gtest.h>
+#include "calculator.h"
 
-class FooTest : public testing::Test {
+struct CalculatorData {
+    int operand1;
+    int operand2;
+    int expectedResult;
+    
+};
+
+class CalculatorTest : public ::testing::TestWithParam<CalculatorData>{
  protected:
   // Per-test-suite set-up.
   // Called before the first test in this test suite.
@@ -31,12 +39,26 @@ class FooTest : public testing::Test {
   
 };
 
+//Data Generation
+INSTANTIATE_TEST_SUITE_P(
+    CalculatAddDataGroup,
+    CalculatorTest,
+    ::testing::Values(
+
+        // All normal
+        CalculatorData{10, 20, 30},
+        CalculatorData{10, 30, 40},
+        CalculatorData{20, 20, 40},
+        CalculatorData{100, 202, 300},
+    )
+);
 
 
-TEST_F(FooTest, Test1) {
+
+TEST_P(CalculatorTest, Test1) {
   
+    onst auto& data = GetParam();
+    int actualResult=add(data.operand1,data.operand2);
+    EXPECT_EQ(actualResult,data.expectedResult);
 }
 
-TEST_F(FooTest, Test2) {
-  
-}
